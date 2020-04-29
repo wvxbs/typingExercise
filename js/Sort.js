b43e25509d879e2b00e8e1792a8535b590edf5c2
wvxbs <gabriel.ferreira7854@gmail.com>
2020-04-29 16:40:18 -0300

    .

--- js/Sort.js
+++ js/Sort.js
@@ -1,11 +1,20 @@
 export const Sort = Arr => {
-    Sorter(Arr)
+    let Sorted = Sorter(Arr)
 
+    return Sorted
 }
+
+let comp = 0
+
+let Sorted = []
 
 const Sorter = Arr => {
     Arr.map(res => {
-        
+        if(res > comp){
+            Sorted.push(comp)
+        }
+
+        comp = res
     })
 
 }
